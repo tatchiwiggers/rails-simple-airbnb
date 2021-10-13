d455ae18dd3b728a859e7ed1947dbcddeadc7ec4
tabatha wiggers <tatisabato@hotmail.com>
2021-10-13 17:29:39 -0300

    adds front end to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+root to: 'flats#home'
+# As a user, I can see all the available flats on our website => index
+# As a user, I can post a flat to the website, specifying its name and address => new/create
+# As a user, I can see detailed information of a given flat => show
+# As a user, I can edit the details of a flat if I made a mistake => edit/update
+# As a user, I can delete a flat from the website, in case I don’t want to rent it anymore => destroy
+resources :flats, except: [ :index ]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.destroy_all
+
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200'
+)
+
+Flat.create!(
+  name: 'Small but cozy apartment in SP',
+  address: '440 Matias Aires Street',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 55,
+  number_of_guests: 2,
+  picture_url: 'https://images.unsplash.com/photo-1493809842364-78817add7ffb?w=1200'
+)
+
+Flat.create!(
+  name: 'Big house with a pool and BBQ area in BH',
+  address: '170 Copernico St',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url: 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?w=1200'
+)
+
+puts 'done!'
